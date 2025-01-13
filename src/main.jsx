be70da923181d247efc9602b646d76adaef0b1e2
Hamza Ahmad <106329588+Hamza-Ahmad2170@users.noreply.github.com>
2025-01-13 23:24:50 +0500

    d

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-// async function f1() {
-//   const res = await fetch("http://localhost:9000/cities");
-//   const data = await res.json();
-//   console.log(data);
-// }
-// f1();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
